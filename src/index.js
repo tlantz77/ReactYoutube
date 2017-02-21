c90b385176ea4dffd1a44809e605c60a7ee1185a
Tim Lantz <tlantz@gmail.com>
2017-02-20 22:30:04 -0600

    implement basic input for search bar

--- src/index.js
+++ src/index.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import SearchBar from './components/search_bar';
+
 const API_KEY = 'AIzaSyBKRpq7ms0lLwUN4is9VCKHAUusKha7X9U';
 
 // Create a new component.  This component should produce HTML.
 const App = () => {
-  return <div>Hi!</div>;
+  return (
+    <div>
+      <SearchBar />
+    </div>;
+  );
 }
 
 // Take this component's generated HTML and put it on the page (in the DOM).

3ad3aa561eee563ce5484feaa12d9e7250733deb
Tim Lantz <tlantz@gmail.com>
2017-02-25 15:53:55 -0600

    click functionality and styling

--- src/index.js
+++ src/index.js
@@ -3,19 +3,24 @@ import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
 import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
 
 const API_KEY = 'AIzaSyBKRpq7ms0lLwUN4is9VCKHAUusKha7X9U';
-
-
 
 class App extends Component {
   constructor(props) {
     super(props);
 
-    this.state = { videos: [] };
+    this.state = {
+      videos: [],
+      selectedVideo:  null
+    };
 
-    YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
-      this.setState({ videos }); // this.setState({ videos: videos });
+    YTSearch({key: API_KEY, term: 'm83 live'}, (videos) => {
+      this.setState({
+        videos: videos,
+        selectedVideo: videos[0]
+       });
     });
   }
 
@@ -23,7 +28,10 @@ class App extends Component {
     return (
       <div>
         <SearchBar />
-        <VideoList videos={this.state.videos} />
+        <VideoDetail video={this.state.selectedVideo} />
+        <VideoList
+          onVideoSelect={selectedVideo => this.setState({selectedVideo}) }
+          videos={this.state.videos} / >
       </div>
     );
   }
